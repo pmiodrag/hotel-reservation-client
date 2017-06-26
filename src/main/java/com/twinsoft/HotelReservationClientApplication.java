83814ba74643cf097bab3a4abf56ac34fe980ad1
miodrag <mpavkovic@gmail.com>
2017-06-26 19:11:18 +0200

    First commit

---
+++ src/main/java/com/twinsoft/HotelReservationClientApplication.java
@@ -0,0 +1,12 @@
+package com.twinsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelReservationClientApplication.class, args);
+	}
+}
